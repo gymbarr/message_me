2184bc10edb9f779aa2d97b163fc97bf05317200
Andrey Timakhovich <stewart96.at@gmail.com>
2022-07-18 15:41:56 +0300

    auto-scrolling to chat window added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,10 +18,18 @@
 //= require semantic-ui
 //= require_tree .
 
+// scroll to the bottom of chatbox function
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
 
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  scroll_bottom();
 })
