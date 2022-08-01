1d1b44574c436913678b4638172b87f773254afc
Andrey Timakhovich <stewart96.at@gmail.com>
2022-08-01 12:48:32 +0300

    migration from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_07_13_151151) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
